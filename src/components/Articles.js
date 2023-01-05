48f41b518910e6445b5cd750d3dd755780bea0ee
Akshit Tyagi <akshit.tyagi@akqa.com>
2023-01-05 13:27:06 +0530

    first commit

---
+++ src/components/Articles.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react'
+import SkeletonArticle from '../skeletons/SkeletonArticle'
+import SkeletonElement from '../skeletons/SkeletonElement'
+
+const Articles = () => {
+    const [articles, setArticles] = useState(null)
+    useEffect(() => {
+        setTimeout(async () => {
+            const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+            const data = await res.json()
+            setArticles(data)
+        }, 5000)
+    })
+  return (
+    <div className='articles'>
+        <h2>Articles</h2>
+        {articles && articles.map(article => (
+            <div className='article' key={ article.id }>
+                <h2>{ article.title }</h2>
+                <p>{ article.body }</p>
+            </div>
+        ))}
+        {!articles && [1,2,3,4,5,6,7,8,9,10].map(n => <SkeletonArticle key={n} theme='light'/>)}
+    </div>
+  )
+}
+
+export default Articles
